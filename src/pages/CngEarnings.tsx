5a1a8e9c10d814b0263512c7fef4bb5e35987133
Debajit91 <debajitroy544@gmail.com>
2025-09-17 18:45:01 +0600

    bike earning car earning & cng earning page created

---
+++ src/pages/CngEarnings.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CngEarnings = () => {
+    return (
+        <div className="p-10">সিএনজি রাইড দিয়ে আয় — শীঘ্রই…</div>
+    );
+};
+
+export default CngEarnings;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="p-10">পৃষ্ঠা খুঁজে পাওয়া যায়নি</div>
+    );
+};
+
+export default NotFound;
