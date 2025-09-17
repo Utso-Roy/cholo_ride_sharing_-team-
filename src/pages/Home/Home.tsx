4f006c472754e6736aa9c4caae18a35de6de0d9a
Utso Roy <royutso003@gmail.com>
2025-09-17 14:29:54 +0600

    added swiper js to the home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SwiperPage from './SwiperPage';
+
+const Home = () => {
+  return (
+    <div>
+      <section>
+        <SwiperPage></SwiperPage>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
