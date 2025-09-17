cb9413ba749592861f99752df482a2d983b9a34c
Debajit91 <debajitroy544@gmail.com>
2025-09-17 18:57:48 +0600

    routes declared in routes file

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import Error from "../Error/Error";
 import Home from "../pages/Home";
 import RootLayout from "../Layout/RootLayout";
+import BikeEarnings from "../pages/BikeEarnings";
+import CarEarnings from "../pages/CarEarnings";
+import CngEarnings from "../pages/CngEarnings";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,18 @@ const router = createBrowserRouter([
         element: <Home></Home>,
         path: "/",
       },
+      {
+        path: "/earn/bike",
+        Component: BikeEarnings
+      },
+      {
+        path: "/earn/car",
+        Component: CarEarnings
+      },
+      {
+        path: "/earn/cng",
+        Component: CngEarnings
+      }
     ],
   },
 ]);
