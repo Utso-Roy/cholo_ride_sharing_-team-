2fbcc1029b37ecddcdcc2375227385f2e387797f
Utso Roy <royutso003@gmail.com>
2025-09-26 11:56:54 +0600

    update popular destination page

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,9 +2,14 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
-  // আরও ভ্যারিয়েবল থাকলে এখানে দিন
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
-}
+} apiKey: "AIzaSyB0ipRcQee-MH1tOcuYNUCc2ZmawMOEyVw",
+  authDomain: "yatri-e22bc.firebaseapp.com",
+  projectId: "yatri-e22bc",
+  storageBucket: "yatri-e22bc.firebasestorage.app",
+  messagingSenderId: "839504356408",
+  appId: "1:839504356408:web:d17da06171de05e954f2cd",
+  measurementId: "G-2J59TRC10P"
